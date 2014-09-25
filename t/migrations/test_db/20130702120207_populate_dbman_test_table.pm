0fd8ade813c8cc76909f280d3e70f378b0c47f02
Craig N. Caroon <ccaroon@webassign.net>
2014-09-25 16:34:16 -0400

    * fixed deprecation warning about use qw(..) as parens.

--- t/migrations/test_db/20130702120207_populate_dbman_test_table.pm
+++ t/migrations/test_db/20130702120207_populate_dbman_test_table.pm
@@ -9,7 +9,7 @@ sub up {
     my ( $class, $dbh ) = @_;
 
     my $count = 0;
-    foreach my $key qw(foo bar baz bop beep dup plop) {
+    foreach my $key (qw|foo bar baz bop beep dup plop|) {
         $dbh->do(
             "insert into dbman_test_table (name,value,is_core) values ('$key', $count, 1)"
         );
@@ -22,7 +22,7 @@ sub up {
 sub down {
     my ( $class, $dbh ) = @_;
 
-    foreach my $key qw(foo bar baz bop beep dup plop) {
+    foreach my $key (qw|foo bar baz bop beep dup plop|) {
         $dbh->do("delete from dbman_test_table where name='$key'");
     }
 
